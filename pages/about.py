cc61f8610b1c0bff526b7f1147a3b308e67215cd
lngalmsyr <lanang.al2610@gmail.com>
2024-05-02 16:10:18 +0700

    Bismillah #2

--- pages/about.py
+++ pages/about.py
@@ -11,19 +11,20 @@ colored_header(
 )
 st.write("""
         Project ini merupakan tugas akhir dari **Lanang Almasyuri**, mahasiswa program studi Sains Data di Institut Teknologi Sumatera. 
-        Project ini bertujuan untuk ... (jelaskan tujuan project Anda).
+        Project ini bertujuan untuk  membangun sebuah model machine learning yang dapat mendeteksi berita palsu (hoax) dalam bahasa Indonesia menggunakan metode fine-tuning pada model IndoBERT.
     """)
 
 st.write("""
         **Kekurangan dan Pengembangan Lebih Lanjut**
 
         Project ini masih dalam tahap pengembangan dan memiliki beberapa kekurangan, di antaranya:
-        * ... (sebutkan beberapa kekurangan project Anda)
-        * ... 
-
+        * Data yang digunakan masih terbatas pada data yang diambil dari website turnbackhoax.id
+        * hasil prediksi masih belum optimal, hanya dapat deteksi berita palsu ini hanya berdasarkan pada judul berita yang ada di dataset. Hasil deteksi ini tidak sepenuhnya akurat dan hanya digunakan sebagai referensi
+        * Usahakan Masukkan Kata Kunci berita, usahakan kata kunci berita yang dimasukkan lebih spesifik agar mendapatkan hasil yang lebih akurat.
+        
         Kami sangat terbuka untuk masukan dan kontribusi dari siapa pun yang ingin mengembangkan project ini lebih lanjut. 
         Kode sumber project ini tersedia di GitHub: 
-        [Link ke Repository GitHub](https://github.com/your-username/your-repository)  
+        [Link ke Repository GitHub](https://github.com/lngalmsyr/hoax-app.git)  
 """)
 
 st.write("""
